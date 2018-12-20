280fbfb12e491a33143e77420c74697c4b618c19
Lucas Lago <lucas.spirit@gmail.com>
2018-12-19 23:20:36 -0200

    Removendo os loops para usar crontab

--- deletados.py
+++ deletados.py
@@ -6,14 +6,13 @@ if __name__ == '__main__':
     import verify_deletion
     from random import shuffle
 
-    while True:
-        arrobas = contas.pega_contas()
-        arrobas.remove("acaciofavacho")
-        shuffle(arrobas)
-        print("Checando se tweets foram removidos...")
-        start_time = time.time()
-        verify_deletion.verify(arrobas)
-        end_time = time.time()
-        print("Terminado em... ")
-        print(end_time - start_time)
+    arrobas = contas.pega_contas()
+    arrobas.remove("acaciofavacho")
+    shuffle(arrobas)
+    print("Checando se tweets foram removidos...")
+    start_time = time.time()
+    verify_deletion.verify(arrobas)
+    end_time = time.time()
+    print("Terminado em... ")
+    print(end_time - start_time)
 
--- update_from_list.py
+++ update_from_list.py
@@ -3,12 +3,8 @@ if __name__ == '__main__':
     import lists
     import arquivar_tweets
 
-    while True:
-        print("Colentado novos tweets...")
-        start_time = time.time()
-        lists.get_new_tweets()
-        arquivar_tweets.arquivar_tweets()
-        end_time = time.time()
-        print(end_time-start_time)
-        if end_time-start_time<60:
-            time.sleep(60 - (end_time - start_time))
+    print("Colentado novos tweets...")
+    start_time = time.time()
+    lists.get_new_tweets()
+    arquivar_tweets.arquivar_tweets()
+    end_time = time.time()
